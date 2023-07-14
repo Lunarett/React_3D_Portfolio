acdc17a18058ca3607bc28bd76d4bf56166efbbe
Egor Ageev <54112318+Lunarett@users.noreply.github.com>
2023-07-14 15:53:38 +0200

    small typo fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacaks, Hero, Navbar, Works, StarsCanvas } from './components';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, StarsCanvas } from './components';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <BrowserRouter>
       <div>Hello World</div>
