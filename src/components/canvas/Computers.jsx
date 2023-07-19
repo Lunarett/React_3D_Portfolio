ff58871076ed5dde1cc93551538d96e50710e081
Egor Ageev <54112318+Lunarett@users.noreply.github.com>
2023-07-19 16:46:41 +0200

    Completed About Section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
     <mesh>
@@ -20,8 +20,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -2, -2]}
+        scale={isMobile ? .4 : .75}
+        position={isMobile ? [-1, -1.5, -1] : [0, -2, -2]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -29,6 +29,23 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addListener(handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    }
+  }, [])
+
   return (
     <Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 30 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
@@ -37,7 +54,7 @@ const ComputersCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
 
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
