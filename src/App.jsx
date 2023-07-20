e4d348dba770f60bfcf38871600c2126c095d381
Egor Ageev <54112318+Lunarett@users.noreply.github.com>
2023-07-20 14:44:47 +0200

    Contact Section Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, StarsCanvas, Tech } from './components';
+import { About, Contact, Experience, Hero, Navbar, StarsCanvas, Tech } from './components';
 
 const App = () => {
   return (
@@ -13,8 +13,6 @@ const App = () => {
         <About />
         <Experience />
         <Tech />
-        <Works />
-        <Feedbacks />
 
         <div className='relative z-0'>
           <Contact />
